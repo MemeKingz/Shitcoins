e81d309490515495d91f1f488950bdf73d75947f
Sam <towsy004@mymail.unisa.edu.au>
2024-06-11 18:09:55 +0930

    update

--- scrape-pumpfun.py
+++ scrape-pumpfun.py
@@ -1,7 +1,6 @@
 from telethon import TelegramClient, events
 import re
 
-
 api_id = '29624477'
 api_hash = '1bb44d4c8505ee865d5df38759d388b5'
 
@@ -13,24 +12,24 @@ def extract_mint_address(text):
         return mint.group(1)
     return None
 
-async def main():
+async def fetch_mint_addresses(limit=100):
     await client.start()
-
-    # Replace 'channel_or_group_username' with the username of the channel/group you want to scrape
+    
     entity = await client.get_entity('pumpfundetector')
 
     mint_addresses = []
 
-    # Fetch the last 100 messages
-    async for message in client.iter_messages(entity, limit=100):
+    async for message in client.iter_messages(entity, limit=limit):
         mint_address = extract_mint_address(message.text)
         if mint_address:
             mint_addresses.append(mint_address)
 
-    # Print the list of mint addresses
-    for address in mint_addresses:
-        print(address)
+    return mint_addresses
 
-# Run the client
+
 with client:
-    client.loop.run_until_complete(main())
+    mint_addresses = client.loop.run_until_complete(fetch_mint_addresses())
+
+
+for address in mint_addresses:
+    print(address)
